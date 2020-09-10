a30e2acff62db44e7093546d8d404d1275860613
Ulrich Schuster <ulrich.schuster@koing.de>
2020-09-10 10:36:18 +0200

    Added option to resume a session from manually input data

--- clairchen.ino
+++ clairchen.ino
@@ -32,10 +32,32 @@
  *
  *******************************************************************************/
 
+// We need to be able to compile with different options without editing source.
+// When building with a more advanced environment, set the following variable:
+// ARDUINO_LMIC_PROJECT_CONFIG_H=my_project_config.h
+// otherwise the lmic_project_config.h from the LMIC library folder will be used
+#ifndef ARDUINO_LMIC_PROJECT_CONFIG_H
+# define ARDUINO_LMIC_PROJECT_CONFIG_H ../../../clairchen/project_config/lmic_project_config.h
+#endif
+
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 
+// Define to resume an existing session without JOINING the TTN again.
+// Requires DeviceAddr, NwSKey, and AppSKey to be defined below.
+#ifndef CONN_RESUME
+# define CONN_RESUME 1
+#endif
+
+// Program Version, to be printed in debug messages.
+// Increment with each change to the code and commit.
+static const u2_t progVer = 2;
+
+
+//--------------------------------------------------------------------
+//--  Keys
+//--------------------------------------------------------------------
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
@@ -53,18 +75,24 @@ void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 static const u1_t PROGMEM APPKEY[16] = { 0x3E, 0x80, 0x01, 0x93, 0x8E, 0x2D, 0x42, 0x27, 0x97, 0x9C, 0xE3, 0x6E, 0x2A, 0x4F, 0x2C, 0x17 };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
-static uint8_t mydata[] = "H";
-static osjob_t sendjob;
+// LoRaWAN NwkSKey, network session key -- To directly connect without performing a JOIN
+// This should be in big-endian (aka msb).
+// Key for Mrs. Red
+static const PROGMEM u1_t NWKSKEY[16] = { 0x04, 0xDC, 0x2E, 0x8A, 0xF8, 0x17, 0xB2, 0xA4, 0xBF, 0x30, 0x8E, 0x72, 0x07, 0x23, 0x0A, 0xD4 };
 
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
-const unsigned TX_INTERVAL = 300;
+// LoRaWAN AppSKey, application session key -- To directly connect without performing a JOIN
+// This should also be in big-endian (aka msb).
+// Key for Mrs. Red
+static const u1_t PROGMEM APPSKEY[16] = { 0x4D, 0x30, 0xF2, 0x8F, 0x17, 0xA6, 0x5F, 0x7C, 0xA6, 0x19, 0x76, 0x69, 0x16, 0x7B, 0xB1, 0x28 };
 
-// Pin mapping
-//
-// Adafruit BSPs are not consistent -- m0 express defs ARDUINO_SAMD_FEATHER_M0,
-// m0 defs ADAFRUIT_FEATHER_M0
-//
+// LoRaWAN end-device address (DevAddr)
+// See http://thethingsnetwork.org/wiki/AddressSpace
+// The library converts the address to network byte order as needed, so this should be in big-endian (aka msb) too.
+static const u4_t DEVADDR = 0x26014C00 ; // Address for Mrs. Red
+
+//--------------------------------------------------------------------
+//--  Board Setup for Adafruit Feather MO LoRa
+//--------------------------------------------------------------------
 #if defined(ARDUINO_SAMD_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0)
 // Pin mapping for Adafruit Feather M0 LoRa, etc.
 const lmic_pinmap lmic_pins = {
@@ -76,38 +104,39 @@ const lmic_pinmap lmic_pins = {
     .rssi_cal = 8,              // LBT cal for the Adafruit Feather M0 LoRa, in dB
     // D-stepping of the silicon, so the 10Mhz SPI clock should work.
 };
-#elif defined(ARDUINO_AVR_FEATHER32U4)
-// Pin mapping for Adafruit Feather 32u4 LoRa, etc.
-// Just like Feather M0 LoRa, but uses SPI at 1MHz; and that's only
-// because MCCI doesn't have a test board; probably higher frequencies
-// will work.
-const lmic_pinmap lmic_pins = {
-    .nss = 8,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 4,
-    .dio = {7, 6, LMIC_UNUSED_PIN},
-    .rxtx_rx_active = 0,
-    .rssi_cal = 8,              // LBT cal for the Adafruit Feather 32U4 LoRa, in dB
-    .spi_freq = 1000000,
-};
-#elif defined(ARDUINO_CATENA_4551)
-// Pin mapping for Murata module / Catena 4551
-const lmic_pinmap lmic_pins = {
-        .nss = 7,
-        .rxtx = 29,
-        .rst = 8,
-        .dio = { 25,    // DIO0 (IRQ) is D25
-                 26,    // DIO1 is D26
-                 27,    // DIO2 is D27
-               },
-        .rxtx_rx_active = 1,
-        .rssi_cal = 10,
-        .spi_freq = 8000000     // 8MHz
-};
 #else
 # error "Unknown target"
 #endif
 
+//--------------------------------------------------------------------
+//--  TX Setup for Testing
+//--------------------------------------------------------------------
+static uint8_t mydata[] = "H";
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Set up the channel scheme used in the EU-868 band plan.
+// They are fixed; hence, there is no need to store and reload them
+void setupEu868Channels() {
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+}
+
+//--------------------------------------------------------------------
+//--  Debug Utility Functions
+//--------------------------------------------------------------------
 void printHex2(unsigned v) {
     v &= 0xff;
     if (v < 16)
@@ -115,6 +144,40 @@ void printHex2(unsigned v) {
     Serial.print(v, HEX);
 }
 
+void printAddrAndKeys() {
+    u4_t netid = 0;
+    devaddr_t devaddr = 0;
+    u1_t nwkKey[16];
+    u1_t artKey[16];
+    LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+    Serial.print(F("netid: "));
+    Serial.println(netid, DEC);
+    Serial.print(F("devaddr: "));
+    Serial.println(devaddr, HEX);
+    Serial.print(F("AppSKey: "));
+    for (size_t i=0; i<sizeof(artKey); ++i) {
+      if (i != 0)
+        Serial.print("-");
+      printHex2(artKey[i]);
+    }
+    Serial.println("");
+    Serial.print(F("NwkSKey: "));
+    for (size_t i=0; i<sizeof(nwkKey); ++i) {
+            if (i != 0)
+                    Serial.print("-");
+            printHex2(nwkKey[i]);
+    }
+    Serial.println();            
+}
+
+void printResumeInfo() {
+  Serial.println(F("Resuming an already established session."));
+  printAddrAndKeys();
+}
+
+//--------------------------------------------------------------------
+//--  Main TX/RX Event Handler
+//--------------------------------------------------------------------
 void onEvent (ev_t ev) {
     Serial.print(os_getTime());
     Serial.print(": ");
@@ -136,31 +199,9 @@ void onEvent (ev_t ev) {
             break;
         case EV_JOINED:
             Serial.println(F("EV_JOINED"));
-            {
-              u4_t netid = 0;
-              devaddr_t devaddr = 0;
-              u1_t nwkKey[16];
-              u1_t artKey[16];
-              LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
-              Serial.print("netid: ");
-              Serial.println(netid, DEC);
-              Serial.print("devaddr: ");
-              Serial.println(devaddr, HEX);
-              Serial.print("AppSKey: ");
-              for (size_t i=0; i<sizeof(artKey); ++i) {
-                if (i != 0)
-                  Serial.print("-");
-                printHex2(artKey[i]);
-              }
-              Serial.println("");
-              Serial.print("NwkSKey: ");
-              for (size_t i=0; i<sizeof(nwkKey); ++i) {
-                      if (i != 0)
-                              Serial.print("-");
-                      printHex2(nwkKey[i]);
-              }
-              Serial.println();
-            }
+            printAddrAndKeys();
+            // Enable automatic data rate adjustment
+            LMIC_setAdrMode(1);
             break;
         /*
         || This event is defined but not used in the code. No
@@ -186,6 +227,10 @@ void onEvent (ev_t ev) {
               Serial.println(LMIC.dataLen);
               Serial.println(F(" bytes of payload"));
             }
+            Serial.print("Uplink Sequence Counter: ");
+            Serial.println(LMIC.seqnoUp, DEC);
+            Serial.print("Downlink Sequence Counter: ");
+            Serial.println(LMIC.seqnoDn, DEC);
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
@@ -263,10 +308,21 @@ void setup() {
     os_init();
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
-  
-    LMIC_startJoining();
 
-    LMIC_setAdrMode(1); 
+    #ifdef CONN_RESUME
+      // Resume an already established connection - from a previous OTAA JOIN.
+      uint8_t appskey[sizeof(APPSKEY)];
+      uint8_t nwkskey[sizeof(NWKSKEY)];
+      memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+      memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+      LMIC_setSession (0x13, DEVADDR, nwkskey, appskey);
+      setupEu868Channels();
+      // Start out with maximum data rate and power. Works in Berlin because of dense TTN coverage.
+      LMIC_setDrTxpow(DR_SF9,14);
+      // Enable automatic data rate adjustment
+      LMIC_setAdrMode(1);
+      printResumeInfo();
+    #endif
   
     // Start job (sending automatically starts OTAA too)
     do_send(&sendjob);
